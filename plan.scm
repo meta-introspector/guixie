92768ab560761e93734a7e4da0ddb8379223b6eb
mike dupont <mike.dupont@introspector.local>
2024-05-30 10:05:30 -0400

    update

--- plan.scm
+++ plan.scm
@@ -1,7 +1,7 @@
 (use-modules (guix)
              (gnu)
              (gnu system)
-             (gnu system activation)
+;;             (gnu system activation)
              (gnu services))
 
 (define (ooda-loop)
@@ -13,15 +13,19 @@
 
   (define (orient)
     ;; Orient the system based on observations and inputs
-    #;... )
+    #;...
+    1)
 
   (define (decide)
     ;; Decide on a course of action based on observations and orientation
-    #;... )
+    #;...
+    1
+    )
 
   (define (act)
     ;; Act on the decision made, updating the system accordingly
-    #;... )
+    #;...
+    1)
   
   ;; Execute the OODA loop
   (define (execute-ooda-loop)
@@ -33,11 +37,23 @@
   ;; Return the OODA loop execution procedure
   execute-ooda-loop)
 
+;; (define cuirass
+;;   ;; Continuous Integration service
+;;   (service cuirass-service-type
+;;            (cuirass-configuration
+;;             (inherit (guix-bootstrap-service-type))
+;;             (source-mirrors '("https://git.example.org")))
+;;            (git-checkout "/srv/git/cuirass"))
+;; )
+
 
 ;; Define the system configuration
 (define guixie
   (operating-system
-    ;; Define system services
+   ;; Define system services
+   (bootloader 1)
+   (host-name "introspector")
+   (file-systems 1)
     (services
       (append
         ;; Guix Bootstrap service
@@ -53,92 +69,96 @@
                        (git-configuration
                          (repos '("/srv/git"))
                          (protocol 'p2p)))
-              ;; Continuous Integration service
-              (service cuirass-service-type
-                       (cuirass-configuration
-                         (inherit (guix-bootstrap-service-type))
-                         (source-mirrors '("https://git.example.org")))
-                       (git-checkout "/srv/git/cuirass")))
+              )
         %base-services)))) ;; Include base system services
 
 ;; Define smart contract-driven implementation driver
 (define (implementation-driver)
   ;; Define smart contract logic here
+  1
   #;... )
 
 ;; Define configuration management with derivations
 (define (configuration-management)
+  1
   ;; Define derivations for system configuration
   #;... )
 
 ;; Define testing and deployment logic
 (define (testing-and-deployment)
+  1
   ;; Define testing and deployment procedures
-  #;... )
+  )
 
 ;; Define community engagement and collaboration
-(define (community-engagement)
-  ;; Define community engagement strategies
-  (define (project-dependencies)
+(define (community-engagement) 1)
+  ;; ;; Define community engagement strategies
+  ;; (define (project-dependencies
+  ;; 	   1
+  ;; 	   )
 
-    (define (consider-some-system-packages)
-      (define (consider-documentation))
-      (define (consider-license))
-      (define (consider-transitive-dependencies)
-	(define (consider-license)
+  ;;   (define (consider-some-system-packages)
+  ;;     (define (consider-documentation)
+  ;; 	1)
+  ;;     (define (consider-license)
+  ;; 	1)
+  ;;     (define (consider-transitive-dependencies)
+  ;; 	(define (consider-other-license)
+  ;; 	  (define (consider-dicotomy-of-permissions)
+  ;; 	    (define (filter-out-non-open-licenses)
+  ;; 	      ;; we will have to make an exception for nvidia, but we can consider that to be a captured node.
+  ;; 	      ;; captured node are not free nodes. 
+  ;; 	      1
+  ;; 	  )
 
-	  (define (consider-dicotomy-of-permissions)
-	    (define (filter-out-non-open-licenses)
-	      ;; we will have to make an exception for nvidia, but we can consider that to be a captured node.
-	      ;; captured node are not free nodes. 
-	      
-	  )
+  ;; 	)
+  ;;     (define (consider-popularity) 1)
+  ;;     (define (consider-code)
 
-	)
-      (define (consider-popularity))
-      (define (consider-code)
+  ;; 	(define (consider-language-scheme) 1)
+  ;; 	(define (consider-language-nix) 1)
+  ;; 	(define (consider-language-rust) 1)
+  ;; 	(define (consider-language-ocaml) 1)
+  ;; 	(define (consider-language-erlang) 1)
+  ;; 	(define (consider-language-elixir) 1)
+  ;; 	(define (consider-language-mojo) 1)
 
-	(define (consider-language-scheme))
-	(define (consider-language-nix))
-	(define (consider-language-rust))
-	(define (consider-language-ocaml))
-	(define (consider-language-erlang))
-	(define (consider-language-elixir))
-	(define (consider-language-mojo))
+  ;; 	(define (consider-language-generically language)
 
-	(define (consider-language-generically language)
-
-	  (define (find-system-packages-for package))
+  ;; 	  (define (find-system-packages-for package) 1)
 	  
-	  (define (consider-some-system-packages package)
+  ;; 	  (define (consider-some-system-packages package)
 
-	    ;; first recursion,
-	    ;; get the package for the language and consider that
-	    (consider-some-system-packages(find-system-packages-for package))	  
-	    )	       
-	)     
-      )
-    (define (consider-guix-packages))
-    (define (consider-nix-packages))
-    (define (consider-docker-packages))
-    (define (consider-helm-packages))
-    )
+  ;; 	    ;; first recursion,
+  ;; 	    ;; get the package for the language and consider that
+  ;; 	    (consider-some-system-packages(find-system-packages-for package))	  
+  ;; 	    )	       
+  ;; 	)     
+  ;;     )
+  ;;   (define (consider-guix-packages) 1)
+  ;;   (define (consider-nix-packages) 1)
+  ;;   (define (consider-docker-packages) 1)
+  ;;   (define (consider-helm-packages) 1)
+  ;;   )
     
-  #;... )
+  ;; 	#;... )
+  ;;     ))
+  ;; 1
+  ;; )
 
 ;; Define the plan to execute
-(define (execute-plan)
-  (operating-system
-    ;; Include system services and extensions
-    (services
-      (append
-        (list (implementation-driver)
-              (configuration-management)
-              (testing-and-deployment)
-              (community-engagement)
-	      (ooda-loop);; Include the OODA loop
-	      )
-        %base-services)))) ;; Include base system services
+;; (define (execute-plan)
+;;   (operating-system
+;;     ;; Include system services and extensions
+;;     (services
+;;       (append
+;;         (list (implementation-driver)
+;;               (configuration-management)
+;;               (testing-and-deployment)
+;;               (community-engagement)
+;; 	      (ooda-loop);; Include the OODA loop
+;; 	      )
+;;         %base-services)))) ;; Include base system services
 
 ;; Define a procedure to extract details from the code
 (define (extract-details)
@@ -180,7 +200,8 @@
 
 
 ;; Execute the plan
-(execute-plan)
+;;(execute-plan)
 
 
 
+(guixie)
